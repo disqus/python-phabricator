3a0b7367bbc0681de9f80216b9321c8e58af0fe1
Burak Yigit Kaya <byk@disqus.com>
2015-12-25 17:27:50 +0200

    Add Python 3 support

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
+import sys
+
 from setuptools import setup, find_packages
+
+tests_requires = ['pytest']
+
+if sys.version_info[:2] < (2, 7):
+    tests_requires.append('unittest2')
+
+if sys.version_info[:2] <= (3, 3):
+    tests_requires.append('mock')
 
 setup(
     name='phabricator',
@@ -12,13 +22,20 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     test_suite='nose.collector',
-    install_requires=[''],
-    tests_require=['nose', 'unittest2', 'mock'],
+    tests_require=tests_requires,
     include_package_data=True,
     classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'Operating System :: OS Independent',
-        'Topic :: Software Development'
+        'Topic :: Software Development',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )
