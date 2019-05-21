2260079abbf7487bf87f6bfa2c21f260c8004bda
sebastiano <sebastiano@thoughtmachine.net>
2019-05-21 16:36:18 +0100

    setup.py based on __version__

---
+++ phabricator/__version__.py
@@ -0,0 +1,13 @@
+# ,------. ,--.             ,--.          ,--.                ,--.
+# |  .--. '|  ,---.  ,--,--.|  |-. ,--.--.`--' ,---. ,--,--.,-'  '-. ,---. ,--.--.
+# |  '--' ||  .-.  |' ,-.  || .-. '|  .--',--.| .--'' ,-.  |'-.  .-'| .-. ||  .--'
+# |  | --' |  | |  |\ '-'  || `-' ||  |   |  |\ `--.\ '-'  |  |  |  ' '-' '|  |
+# `--'     `--' `--' `--`--' `---' `--'   `--' `---' `--`--'  `--'   `---' `--'
+
+__name__ = 'phabricator'
+__version__ = '0.7.0'
+__author__ = 'Disqus'
+__author_email__ = 'opensource@disqus.com'
+__url__ = 'http://github.com/disqus/python-phabricator'
+__description__ = 'Phabricator API Bindings'
+__license__ = 'Apache License 2.0'
--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
-import sys
 import os
+import sys
 
 from setuptools import setup, find_packages
 
-tests_requires = []
+
+here = os.path.abspath(os.path.dirname(__file__))
+
 
 def requirements2list(pfi_txt='requirements.txt'):
     here = os.path.dirname(os.path.realpath(__file__))
@@ -15,19 +17,31 @@ def requirements2list(pfi_txt='requirements.txt'):
         list_reqs.append(line.replace('\n', ''))
     return list_reqs
 
+
+tests_requires = []
+
+
 if sys.version_info[:2] < (2, 7):
     tests_requires.append('unittest2')
+
 
 if sys.version_info[:2] <= (3, 3):
     tests_requires.append('mock')
 
+
+about = {}
+with open(os.path.join(here, 'phabricator', '__version__.py'), 'r') as f:
+    exec(f.read(), about)
+
+
 setup(
-    name='phabricator',
-    version='0.7.0',
-    author='Disqus',
-    author_email='opensource@disqus.com',
-    url='http://github.com/disqus/python-phabricator',
-    description='Phabricator API Bindings',
+    name=about['__name__'],
+    version=about['__version__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    description=about['__description__'],
+    license=about['__license__'],
     packages=find_packages(),
     zip_safe=False,
     test_suite='tests.test_phabricator',
